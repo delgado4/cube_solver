24a9ea979e1ac5872fe24d93b19d95ba3e9dcdfc
Dominic Delgado <dominicdelgado@Dominics-MacBook-Pro.local>
2015-05-23 16:52:21 -0700

    C Module Completed!

---
+++ system_globals.h
@@ -0,0 +1,6 @@
+#ifndef __SYSTEM_GLOBALS_H__
+#define __SYSTEM_GLOBALS_H__
+
+
+
+#endif /* __SYSTEM_GLOBALS_H__ */
---
+++ test_image.h
@@ -0,0 +1,7 @@
+#ifndef __TEST_IMAGE_H__
+#define __TEST_IMAGE_H__
+
+#include "inttypes.h"
+
+
+#endif /* __TEST_IMAGE_H__ */

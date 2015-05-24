35c5d115a4f2743fce06bc128e794e5e509722cb
delgado4 <delgado4@RM128-6.stanford.edu>
2015-05-23 17:19:52 -0700

    Removed unnecessary files

--- system_globals.h
+++
@@ -1,6 +0,0 @@
-#ifndef __SYSTEM_GLOBALS_H__
-#define __SYSTEM_GLOBALS_H__
-
-
-
-#endif /* __SYSTEM_GLOBALS_H__ */
--- test_image.h
+++
@@ -1,7 +0,0 @@
-#ifndef __TEST_IMAGE_H__
-#define __TEST_IMAGE_H__
-
-#include "inttypes.h"
-
-
-#endif /* __TEST_IMAGE_H__ */
